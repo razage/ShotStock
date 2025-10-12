098e85eb4b617c925ae30b5616db71c9cf3a5946
Thomas Hinkle <thomashinkle2393@gmail.com>
2025-10-12 11:33:02 -0500

    Added the filter form to an accordion and began styling it.

--- client/src/components/FilterForm.tsx
+++ client/src/components/FilterForm.tsx
@@ -1,5 +1,9 @@
 import { Formik, Form, Field } from "formik";
-import { TextField, Button, Box } from "@mui/material";
+import { TextField, Button, Typography } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { FilterAlt } from "@mui/icons-material";
 import * as Yup from "yup";
 
 interface FilterValues {
@@ -42,81 +46,92 @@ function FilterForm({ initialValues, onSubmit }: FilterFormProps) {
             }}
         >
             {({ errors, touched }) => (
-                <Form>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: 2,
-                            maxWidth: 400,
-                        }}
+                <Accordion className="filter-form">
+                    <AccordionSummary
+                        expandIcon={<FilterAlt />}
+                        aria-controls="panel1-content"
+                        id="panel1-header"
                     >
-                        <Field
-                            name="manufacturer"
-                            as={TextField}
-                            label="Manufacturer"
-                            error={
-                                touched.manufacturer && !!errors.manufacturer
-                            }
-                            helperText={
-                                touched.manufacturer && errors.manufacturer
-                            }
-                        />
-                        <Field
-                            name="productLine"
-                            as={TextField}
-                            label="Product Line"
-                            error={touched.productLine && !!errors.productLine}
-                            helperText={
-                                touched.productLine && errors.productLine
-                            }
-                        />
-                        <Field
-                            name="ammoType"
-                            as={TextField}
-                            label="Ammo Type"
-                            error={touched.ammoType && !!errors.ammoType}
-                            helperText={touched.ammoType && errors.ammoType}
-                        />
-                        <Field
-                            name="bulletType"
-                            as={TextField}
-                            label="Bullet Type"
-                            error={touched.bulletType && !!errors.bulletType}
-                            helperText={touched.bulletType && errors.bulletType}
-                        />
-                        <Field
-                            name="bulletWeight"
-                            as={TextField}
-                            label="Bullet Weight (grains)"
-                            type="number"
-                            error={
-                                touched.bulletWeight && !!errors.bulletWeight
-                            }
-                            helperText={
-                                touched.bulletWeight && errors.bulletWeight
-                            }
-                        />
-                        <Field
-                            name="caseMaterial"
-                            as={TextField}
-                            label="Case Material"
-                            error={
-                                touched.caseMaterial && !!errors.caseMaterial
-                            }
-                            helperText={
-                                touched.caseMaterial && errors.caseMaterial
-                            }
-                        />
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                        >
-                            Apply Filters
-                        </Button>
-                    </Box>
-                </Form>
+                        <Typography>Filter</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Form>
+                            <Field
+                                name="manufacturer"
+                                as={TextField}
+                                label="Manufacturer"
+                                error={
+                                    touched.manufacturer &&
+                                    !!errors.manufacturer
+                                }
+                                helperText={
+                                    touched.manufacturer && errors.manufacturer
+                                }
+                            />
+                            <Field
+                                name="productLine"
+                                as={TextField}
+                                label="Product Line"
+                                error={
+                                    touched.productLine && !!errors.productLine
+                                }
+                                helperText={
+                                    touched.productLine && errors.productLine
+                                }
+                            />
+                            <Field
+                                name="ammoType"
+                                as={TextField}
+                                label="Ammo Type"
+                                error={touched.ammoType && !!errors.ammoType}
+                                helperText={touched.ammoType && errors.ammoType}
+                            />
+                            <Field
+                                name="bulletType"
+                                as={TextField}
+                                label="Bullet Type"
+                                error={
+                                    touched.bulletType && !!errors.bulletType
+                                }
+                                helperText={
+                                    touched.bulletType && errors.bulletType
+                                }
+                            />
+                            <Field
+                                name="bulletWeight"
+                                as={TextField}
+                                label="Bullet Weight (gr)"
+                                type="number"
+                                error={
+                                    touched.bulletWeight &&
+                                    !!errors.bulletWeight
+                                }
+                                helperText={
+                                    touched.bulletWeight && errors.bulletWeight
+                                }
+                            />
+                            <Field
+                                name="caseMaterial"
+                                as={TextField}
+                                label="Case Material"
+                                error={
+                                    touched.caseMaterial &&
+                                    !!errors.caseMaterial
+                                }
+                                helperText={
+                                    touched.caseMaterial && errors.caseMaterial
+                                }
+                            />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                            >
+                                Apply Filters
+                            </Button>
+                        </Form>
+                    </AccordionDetails>
+                </Accordion>
             )}
         </Formik>
     );
