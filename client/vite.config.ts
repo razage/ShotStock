ae2e95494ba80a5b1655c1ed494eee657021d89e
Thomas Hinkle <thomashinkle2393@gmail.com>
2025-11-17 12:38:26 -0600

    Paths are now shared between vite and tsconfig. No more errors in VS Code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
             "@styles": path.resolve(__dirname, "./src/styles"),
             "@components": path.resolve(__dirname, "./src/components"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@types": path.resolve(__dirname, "./src/types"),
         },
     },
     css: {
