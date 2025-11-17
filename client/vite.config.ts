7ca59814f398019247ed7d745eada6ada45ac706
Thomas Hinkle <thomashinkle2393@gmail.com>
2025-11-17 12:39:36 -0600

    Updated dependencies and added paths to the server tsconfig.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,20 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import postcssPresetEnv from "postcss-preset-env";
 import cssnano from "cssnano";
-import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-            "@styles": path.resolve(__dirname, "./src/styles"),
-            "@components": path.resolve(__dirname, "./src/components"),
-            "@pages": path.resolve(__dirname, "./src/pages"),
-            "@hooks": path.resolve(__dirname, "./src/hooks"),
-            "@types": path.resolve(__dirname, "./src/types"),
-        },
-    },
+    plugins: [react(), tsconfigPaths()],
     css: {
         preprocessorOptions: {
             less: {
