81e7498100cc342b48a0d4ce30eb7fcce9de6513
Thomas Hinkle <thomashinkle2393@gmail.com>
2025-11-17 11:03:17 -0600

    updated configs with some defined routes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import postcssPresetEnv from "postcss-preset-env";
 import cssnano from "cssnano";
+import path from "node:path";
 
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@styles": path.resolve(__dirname, "./src/styles"),
+            "@components": path.resolve(__dirname, "./src/components"),
+        },
+    },
     css: {
         preprocessorOptions: {
             less: {
